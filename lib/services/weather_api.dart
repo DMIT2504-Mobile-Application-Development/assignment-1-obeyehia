e65cfac50faedd3e950cb7d24b9285ef8a4bf4d6
obeyehia <obedayehia1999@gmail.com>
2025-05-21 17:01:56 -0600

    Inside the getWeatherForCity function, I used Uri.parse to build the full API request URL by combining the OpenWeather endpoint, metric units, the user-provided city, and my API key. This creates a properly formatted URL that can be used to make a safe and valid HTTP GET request.

--- lib/services/weather_api.dart
+++ lib/services/weather_api.dart
@@ -3,3 +3,9 @@ import 'package:http/http.dart' as http;
 
 const String weatherApiKey = '6a9fc1701ca40a318de50f6a99eeb008';
 const String currentWeatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather';
+
+Future<dynamic> getWeatherForCity({required String city}) async {
+  try {
+    final url = Uri.parse('$currentWeatherEndpoint?units=metric&q=$city&appid=$weatherApiKey');
+  }
+}
