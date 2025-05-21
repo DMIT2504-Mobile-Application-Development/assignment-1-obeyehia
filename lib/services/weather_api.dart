80cad37d5e0a1d6f1e0ef1aa1ee2d4afd3bd12e2
obeyehia <obedayehia1999@gmail.com>
2025-05-21 16:16:33 -0600

    added the current weather endpoint exported as a constant named currentWeatherEndpoint with the value that matches the provided format

---
+++ lib/services/weather_api.dart
@@ -0,0 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const String weatherApiKey = '6a9fc1701ca40a318de50f6a99eeb008';
+const String currentWeatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather';
