fee2ca9509b226f161068829738bc98ab33974b7
obeyehia <obedayehia1999@gmail.com>
2025-05-21 18:01:51 -0600

    This part makes the actual request to the weather API. If it works and gives a 200 status, it decodes the JSON and returns the data. If it doesn't, it throws an error saying what went wrong. I used a try/catch just in case something else fails, like no internet or bad data, so it doesn’t crash the whole program.

--- lib/services/weather_api.dart
+++ lib/services/weather_api.dart
@@ -5,11 +5,19 @@ const String weatherApiKey = '6a9fc1701ca40a318de50f6a99eeb008';
 const String currentWeatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather';
 
 Future<dynamic> getWeatherForCity({required String city}) async {
-  try {
-    final url = Uri.parse('$currentWeatherEndpoint?units=metric&q=$city&appid=$weatherApiKey');
 
+  try {
+
+    final url = Uri.parse('$currentWeatherEndpoint?units=metric&q=$city&appid=$weatherApiKey');
 
     final response = await http.get(url);
 
+    if(response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('There was a problem with the request: status ${response.statusCode} received');
+    }
+  } catch (e) {
+    throw Exception('There was a problem with the request: $e');
   }
 }
