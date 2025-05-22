9453cf31105c6093702f7a48986d086a6a693df2
obeyehia <obedayehia1999@gmail.com>
2025-05-21 21:12:27 -0600

    added public getters for all private fields using Dart's arrow function syntax for cleaner access

--- lib/models/current_weather.dart
+++ lib/models/current_weather.dart
@@ -1,18 +1,29 @@
 class CurrentWeather {
-late String _city;
-late String _description;
-late double _currentTemp;
-late DateTime _currentTime;
-late DateTime _sunrise;
-late DateTime _sunset;
+  late String _city;
+  late String _description;
+  late double _currentTemp;
+  late DateTime _currentTime;
+  late DateTime _sunrise;
+  late DateTime _sunset;
 
-CurrentWeather(String city, String description, double currentTemp,
-    DateTime currentTime, DateTime sunrise, DateTime sunset ) {
-this.city = city;
-this.description = description;
-this.currentTemp = currentTemp;
-this.currentTime = currentTime;
-this.sunrise = sunrise;
-this.sunset = sunset;
+  CurrentWeather(String city, String description, double currentTemp,
+      DateTime currentTime, DateTime sunrise, DateTime sunset) {
+    this.city = city;
+    this.description = description;
+    this.currentTemp = currentTemp;
+    this.currentTime = currentTime;
+    this.sunrise = sunrise;
+    this.sunset = sunset;
+  }
+
+  String get city => _city;
+  String get description => _description;
+  double get currentTemp => _currentTemp;
+  DateTime get currentTime => _currentTime;
+  DateTime get sunrise => _sunrise;
+  DateTime get sunset => _sunset;
+
+
+
 }
 
